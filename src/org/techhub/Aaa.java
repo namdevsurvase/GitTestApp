97632594d0a40fb3456c5587d292b5e5cac9cf99
namdevsurvase <nsurvase230@gmail.com>
2025-08-04 16:50:53 +0530

    my Aaaa class file modify

--- src/org/techhub/Aaa.java
+++ src/org/techhub/Aaa.java
@@ -3,5 +3,9 @@ package org.techhub;
 public class Aaa {
 public static void main(String[] args) {
 	System.out.println("Sanaju bbahi");
+		System.out.println("Sanaju bbahi");
+			System.out.println("Sanaju bbahi");
+				System.out.println("Sanaju bbahi");
+					System.out.println("Sanaju bbahi");
 }
 }
